dab58300ae7a6978511a48984747a29eb35bc3b2
Roland Bernard <rolli.bernard@gmail.com>
2025-08-27 15:08:47 +0200

    Improved the multi-sliding windows bucket operator.

--- app/processor/src/main/java/com/rolandb/MultiSlidingBuckets.java
+++ app/processor/src/main/java/com/rolandb/MultiSlidingBuckets.java
@@ -5,9 +5,11 @@ import org.apache.flink.api.common.state.MapState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.PrimitiveArrayTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
+import org.apache.flink.util.Preconditions;
 
 import java.io.Serializable;
 import java.time.Duration;
@@ -18,7 +20,14 @@ import java.util.List;
  * A {@link KeyedProcessFunction} that can efficiently collect multiple sliding
  * windows of different lengths at once. All windows share the same hop distance
  * but can have a different size. Note that the sizes of the windows is
- * restricted to be a multiple of the hop duration.
+ * restricted to be a multiple of the hop duration. Note that this
+ * implementation may send multiple events for the same window, updating it with
+ * late arriving events. As such the output of this operator should be used as
+ * an upsert with the key and window as the primary key.
+ * Important: reconfiguration of the operator is not supported. If old state
+ * values are loaded from a checkpoint, the results might be wrong if the window
+ * sizes or slide duration are not the same. Names or collection function may
+ * be changed however.
  */
 public class MultiSlidingBuckets<K, E, R> extends KeyedProcessFunction<K, E, R> {
     public static class WindowSpec implements Serializable {
@@ -37,67 +46,86 @@ public class MultiSlidingBuckets<K, E, R> extends KeyedProcessFunction<K, E, R> 
 
     private final long slideMs;
     private final List<WindowSpec> windows;
-    private final long maxWindowMs;
     private final ResultFunction<K, R> function;
 
     // Buckets by Timestamp
+    private static final MapStateDescriptor<Long, Long> bucketsDesc = new MapStateDescriptor<>(
+            "bucketCounts", TypeInformation.of(Long.class), TypeInformation.of(Long.class));
     private transient MapState<Long, Long> bucketCounts;
     // Rolling Sum per Window
+    private static final ValueStateDescriptor<long[]> totalsDesc = new ValueStateDescriptor<>(
+            "windowTotals", PrimitiveArrayTypeInfo.LONG_PRIMITIVE_ARRAY_TYPE_INFO);
     private transient ValueState<long[]> windowTotals;
     // When the last window was closed.
+    private static final ValueStateDescriptor<Long> lastTimerDesc = new ValueStateDescriptor<>(
+            "lastClosing", TypeInformation.of(Long.class));
     private transient ValueState<Long> lastTimer;
 
     public MultiSlidingBuckets(Duration slide, List<WindowSpec> windows, ResultFunction<K, R> function) {
         this.slideMs = slide.toMillis();
-        this.windows = windows;
-        this.maxWindowMs = windows.stream().mapToLong(w -> w.sizeMs).max().orElse(slideMs);
+        // We sort the windows, so the first is the shortest and the last is the
+        // longers. The sort is stable so we get the same result each time.
+        this.windows = windows.stream().sorted((a, b) -> Long.compare(a.sizeMs, b.sizeMs)).toList();
         this.function = function;
+        // We only support exact multiples of the slide duration.
         windows.stream().forEach(w -> {
-            // We only support exact multiples of the slide duration.
-            assert w.sizeMs % slideMs == 0;
+            Preconditions.checkArgument(
+                    w.sizeMs % slideMs == 0, "Window size must be multiple of slide duration");
         });
     }
 
     @Override
     public void open(OpenContext parameters) throws Exception {
-        MapStateDescriptor<Long, Long> bucketsDesc = new MapStateDescriptor<>(
-                "bucketCounts", TypeInformation.of(Long.class), TypeInformation.of(Long.class));
-        ValueStateDescriptor<long[]> totalsDesc = new ValueStateDescriptor<>(
-                "windowTotals", TypeInformation.of(long[].class));
-        ValueStateDescriptor<Long> lastTimerDesc = new ValueStateDescriptor<>(
-                "lastClosing", TypeInformation.of(Long.class));
         bucketCounts = getRuntimeContext().getMapState(bucketsDesc);
         windowTotals = getRuntimeContext().getState(totalsDesc);
         lastTimer = getRuntimeContext().getState(lastTimerDesc);
+        // Quick sanity check. Necessary but not sufficient.
+        long[] lastTotals = windowTotals.value();
+        Preconditions.checkState(lastTotals == null || lastTotals.length == windows.size(),
+                "The stored state size does not match the window specifications");
+    }
+
+    private void addNextBucketTimer(Context ctx, long bucketEnd, Long lastClose) throws Exception {
+        // There is always one timer per bucket at one time. Some of the timers
+        // of different buckets may overlap, but that is fine in Flink.
+        if (lastClose == null || bucketEnd > lastClose) {
+            // Register an event-time timer for when this bucket needs to be added
+            // to the totals. In the onTimer for this we will add another timer.
+            ctx.timerService().registerEventTimeTimer(bucketEnd);
+        } else {
+            // Since this bucket has already been added to the different windows
+            // add instead a timer for when it gets removed from the first window.
+            for (WindowSpec spec : windows) {
+                long size = spec.sizeMs / slideMs;
+                if (bucketEnd + size * slideMs > lastClose) {
+                    ctx.timerService().registerEventTimeTimer(bucketEnd + size * slideMs);
+                    return;
+                }
+            }
+            // I we get here, can remove now the bucket since there is no more
+            // any window total that needs it for updating. Note that for every
+            // bucket we go through all of the steps, before removing it completely.
+            bucketCounts.remove(bucketEnd);
+        }
     }
 
     @Override
     public void processElement(E event, Context ctx, Collector<R> out) throws Exception {
-        long ts = ctx.timestamp();
+        long ts = Preconditions.checkNotNull(ctx.timestamp(), "Event timestamp must be assigned");
         long bucketEnd = (ts / slideMs + 1) * slideMs;
         // Check if this bucket still has to be updated. Not that if the last
         // close will have removed the bucket at `lastClose - maxWindowsSize * slideMs`,
         // so if this event is even older we should completely discard it.
         Long lastClose = lastTimer.value();
-        long maxWindowsSize = maxWindowMs / slideMs;
+        long maxWindowsSize = windows.get(windows.size() - 1).sizeMs / slideMs;
         if (lastClose == null || bucketEnd > lastClose - maxWindowsSize * slideMs) {
             Long current = bucketCounts.get(bucketEnd);
             bucketCounts.put(bucketEnd, current == null ? 1 : current + 1);
             // We only need to register new timers if this is a new bucket. Otherwise
-            // those timers already exist.
+            // some timer will already exist. For every bucket we will only have
+            // one timer at a time.
             if (current == null) {
-                // Register an event-time timer for when this bucket ends.
-                if (lastClose == null || bucketEnd > lastClose) {
-                    ctx.timerService().registerEventTimeTimer(bucketEnd);
-                }
-                // For each window size also register an event for when the bucket has
-                // to be removed from the running totals.
-                for (WindowSpec spec : windows) {
-                    long size = spec.sizeMs / slideMs;
-                    if (lastClose == null || bucketEnd + size * slideMs > lastClose) {
-                        ctx.timerService().registerEventTimeTimer(bucketEnd + size * slideMs);
-                    }
-                }
+                addNextBucketTimer(ctx, bucketEnd, lastClose);
             }
             // If this bucket has already been added to some of the window totals,
             // we need to update the value in the totals. We will unpublish a new
@@ -129,6 +157,9 @@ public class MultiSlidingBuckets<K, E, R> extends KeyedProcessFunction<K, E, R> 
         Long addedCount = bucketCounts.get(bucketEnd);
         if (addedCount == null) {
             addedCount = 0L;
+        } else {
+            // This bucket must be removed again later.
+            addNextBucketTimer(ctx, bucketEnd, bucketEnd);
         }
         long[] totals = windowTotals.value();
         if (totals == null) {
@@ -136,23 +167,26 @@ public class MultiSlidingBuckets<K, E, R> extends KeyedProcessFunction<K, E, R> 
         }
         boolean allZero = true;
         for (int i = 0; i < totals.length; i++) {
-            totals[i] += addedCount;
             WindowSpec spec = windows.get(i);
-            long size = spec.sizeMs / slideMs;
-            long expiredBucketEnd = bucketEnd - size * slideMs;
+            long expiredBucketEnd = bucketEnd - (spec.sizeMs / slideMs) * slideMs;
             Long expiredCount = bucketCounts.get(expiredBucketEnd);
             if (expiredCount == null) {
                 expiredCount = 0L;
-            }
-            totals[i] -= expiredCount;
-            if (totals[i] != 0) {
-                allZero = false;
+            } else {
+                // There is a bucket here. We have to make sure we have a timer
+                // for the next time we use it, or remove it if we don't use it
+                // in any window anymore.
+                addNextBucketTimer(ctx, expiredBucketEnd, bucketEnd);
             }
             if (addedCount != expiredCount) {
+                totals[i] += addedCount - expiredCount;
                 out.collect(function.apply(
                         Instant.ofEpochMilli(expiredBucketEnd),
                         Instant.ofEpochMilli(bucketEnd),
                         ctx.getCurrentKey(), spec, totals[i]));
+            }
+            if (totals[i] != 0) {
+                allZero = false;
             }
         }
         if (allZero) {
@@ -161,12 +195,5 @@ public class MultiSlidingBuckets<K, E, R> extends KeyedProcessFunction<K, E, R> 
         } else {
             windowTotals.update(totals);
         }
-        // We can remove now the bucket that was just expired in the longest
-        // window. Clearly the shorter windows had this bucket expire before,
-        // and so it is no longer needed for any window. Since in the element
-        // handling we setup a timer for every window size, this will remove
-        // for every bucket that we ever write to eventually.
-        long maxWindowsSize = maxWindowMs / slideMs;
-        bucketCounts.remove(bucketEnd - maxWindowsSize * slideMs);
     }
 }
