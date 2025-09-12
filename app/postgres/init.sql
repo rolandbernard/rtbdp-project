47d9ec15907d06e204bd0b0d207838f3520d688a
Roland Bernard <rolli.bernard@gmail.com>
2025-09-12 19:14:45 +0200

    Replaced vanilla postgres with timescaledb, and create partitions for history and events table.

--- app/postgres/init.sql
+++ app/postgres/init.sql
@@ -1,4 +1,10 @@
 -- This file contains the schema for the PostgreSQL database. The database will
+-- be filled from the Flink processor and enable the frontend te obtain the latest
+-- state without replaying all of the messages from Kafka.
+
+-- We use TimescaleDB for easier handling of partitioned tables and automatic
+-- retention period.
+CREATE EXTENSION timescaledb;
 
 CREATE TYPE event_kind AS ENUM (
     'all', 'push', 'watch', 'create_repo', 'create_branch', 'create_tag',
@@ -9,6 +15,30 @@ CREATE TYPE event_kind AS ENUM (
 CREATE TYPE window_size AS ENUM (
     '5m', '1h', '6h', '24h'
 );
+
+-- These are not the raw events, but the processed ones that contain only enough
+-- information to display in the stream on the frontend.
+CREATE TABLE events (
+    id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    kind event_kind NOT NULL,
+    repo_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    details TEXT NOT NULL,
+    seq_num BIGINT NOT NULL,
+    PRIMARY KEY (created_at, id)
+);
+
+CREATE INDEX ON events(kind);
+CREATE INDEX ON events(repo_id);
+CREATE INDEX ON events(user_id);
+
+-- This is a high volume table, so we setup a partition based on the creation
+-- timestamp and then a retention period of one month.
+-- ~100 events/second * 60 seconds/minute * 10 minutes/partition = 60000 /partition
+SELECT create_hypertable('events', by_range('created_at', INTERVAL '10 minutes'));
+-- We only keep one month worth of events (~250M events), due to the volume of data.
+SELECT add_retention_policy('events', INTERVAL '30 days');
 
 CREATE TABLE counts_live (
     kind event_kind NOT NULL,
@@ -35,3 +65,9 @@ CREATE TABLE counts_history (
     seq_num BIGINT NOT NULL,
     PRIMARY KEY (kind, ts_start, ts_end)
 );
+
+-- We partition by week. This table does not contain such a large amount of data
+-- so we can use a larger time period.
+-- ~20 kinds * 0.2 events/kind/minute * 60 minutes/hour * 24 hours/day * 7 days/partition = 40320 /partition
+SELECT create_hypertable('counts_history', by_range('ts_start', INTERVAL '7 day'));
+
