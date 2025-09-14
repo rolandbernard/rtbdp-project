e7c20f6a8e68152549427d13bab297ea777cce30
Roland Bernard <rolli.bernard@gmail.com>
2025-09-14 17:38:11 +0200

    Avoid redundant generation of keys and values entries in json serialization in the processor.

--- app/processor/src/main/java/com/rolandb/SequencedRow.java
+++ app/processor/src/main/java/com/rolandb/SequencedRow.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.rolandb.AbstractTable.TableEventKey;
 
@@ -28,10 +29,12 @@ public class SequencedRow {
         }
     }
 
+    @JsonIgnore
     public List<?> getKey() {
         return readKeyFrom(this);
     }
 
+    @JsonIgnore
     public Object[] getValues() {
         return readValuesFrom(this);
     }
